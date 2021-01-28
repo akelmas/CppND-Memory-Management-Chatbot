dc9016ee825562dc0ccd9e08c9ca0f0506d561b6
elmas <abdulkerim.elmas@agu.edu.tr>
2021-01-27 22:47:16 -0800

    INIT

---
+++ build/CMakeFiles/membot.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/elmas/Desktop/dev/CppND/CppND-Memory-Management-Chatbot/src/chatbot.cpp" "/home/elmas/Desktop/dev/CppND/CppND-Memory-Management-Chatbot/build/CMakeFiles/membot.dir/src/chatbot.cpp.o"
+  "/home/elmas/Desktop/dev/CppND/CppND-Memory-Management-Chatbot/src/chatgui.cpp" "/home/elmas/Desktop/dev/CppND/CppND-Memory-Management-Chatbot/build/CMakeFiles/membot.dir/src/chatgui.cpp.o"
+  "/home/elmas/Desktop/dev/CppND/CppND-Memory-Management-Chatbot/src/chatlogic.cpp" "/home/elmas/Desktop/dev/CppND/CppND-Memory-Management-Chatbot/build/CMakeFiles/membot.dir/src/chatlogic.cpp.o"
+  "/home/elmas/Desktop/dev/CppND/CppND-Memory-Management-Chatbot/src/graphedge.cpp" "/home/elmas/Desktop/dev/CppND/CppND-Memory-Management-Chatbot/build/CMakeFiles/membot.dir/src/graphedge.cpp.o"
+  "/home/elmas/Desktop/dev/CppND/CppND-Memory-Management-Chatbot/src/graphnode.cpp" "/home/elmas/Desktop/dev/CppND/CppND-Memory-Management-Chatbot/build/CMakeFiles/membot.dir/src/graphnode.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "WXUSINGDLL"
+  "_FILE_OFFSET_BITS=64"
+  "__WXGTK__"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/lib/x86_64-linux-gnu/wx/include/gtk3-unicode-3.0"
+  "/usr/include/wx-3.0"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
